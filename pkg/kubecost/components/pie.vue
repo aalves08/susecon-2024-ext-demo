dad743d9f629c09d67c2b2784648bac3e6768323
Alexandre Alves <alexandre.alves@suse.com>
2024-06-12 14:10:58 +0100

    commit project

---
+++ pkg/kubecost/components/pie.vue
@@ -0,0 +1,121 @@
+<script>
+import { Pie } from 'vue-chartjs/legacy';
+
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+  CategoryScale
+} from 'chart.js';
+
+ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale);
+
+export default {
+  name:       'PieChart',
+  components: { Pie },
+  props:      {
+    labels: {
+      type:    Array,
+      default: () => []
+    },
+    data: {
+      type:    Array,
+      default: () => []
+    },
+    backgroundColor: {
+      type:    Array,
+      default: () => []
+    },
+    costs: {
+      type:    Array,
+      default: () => []
+    },
+    chartId: {
+      type:    String,
+      default: 'pie-chart'
+    },
+    datasetIdKey: {
+      type:    String,
+      default: 'label'
+    },
+    width: {
+      type:    Number,
+      default: 400
+    },
+    height: {
+      type:    Number,
+      default: 400
+    },
+    cssClasses: {
+      default: '',
+      type:    String
+    },
+    styles: {
+      type:    Object,
+      default: () => {}
+    },
+    plugins: {
+      type:    Array,
+      default: () => []
+    }
+  },
+  computed: {
+    chartData() {
+      let labels;
+      let data;
+      const backgroundColor = [];
+
+      if (this.costs.length) {
+        labels = this.costs.filter(item => item.name !== 'Total').map(item => item.name);
+        data = this.costs.filter(item => item.name !== 'Total').map(item => item.rawValue);
+
+        for (let i = 0; i < data.length; i++) {
+          backgroundColor.push(`#${ Math.floor(Math.random() * 16777215).toString(16) }`);
+        }
+      }
+
+      return {
+        labels,
+        datasets: [
+          {
+            backgroundColor,
+            data,
+          }
+        ]
+      };
+    }
+  },
+
+  data() {
+    return {
+      chartOptions: {
+        responsive:          true,
+        maintainAspectRatio: false
+      }
+    };
+  }
+};
+</script>
+
+<template>
+  <Pie
+    class="custom-pie-chart"
+    :chart-options="chartOptions"
+    :chart-data="chartData"
+    :chart-id="chartId"
+    :dataset-id-key="datasetIdKey"
+    :plugins="plugins"
+    :css-classes="cssClasses"
+    :styles="styles"
+    :width="width"
+    :height="height"
+  />
+</template>
+
+<style lang="scss" scoped>
+.custom-pie-chart {
+  margin-top: 40px;
+}
+</style>
